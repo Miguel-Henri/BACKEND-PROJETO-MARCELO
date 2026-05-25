557776434160590f318b216f41f2200d0010a7c8
unknowmistery16 <miguelhbatista@gmail.com>
2026-05-25 19:58:11 -0300

    first-commit

---
+++ src/main/java/ifsp/edu/br/IFBANK/IfbankApplication.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.IFBANK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifsp/edu/br/IFBANK/IfbankApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.edu.br.IFBANK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
